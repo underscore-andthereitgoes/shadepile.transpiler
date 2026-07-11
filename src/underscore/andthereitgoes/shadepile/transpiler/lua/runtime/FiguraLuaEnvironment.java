f045c60adc2fb296d2a74991d582249c2e3ee31b
_andthereitgoes <adrian.grundy09@gmail.com>
2026-07-11 14:09:11 +1000

    basically no change

--- src/underscore/andthereitgoes/shadepile/transpiler/lua/runtime/FiguraLuaEnvironment.java
+++
@@ -1,7 +0,0 @@
-package underscore.andthereitgoes.shadepile.transpiler.lua.runtime;
-
-public class FiguraLuaEnvironment extends LuaEnvironment {
-  public FiguraLuaEnvironment(LuaRuntime runtime) {
-    super(runtime);
-  }
-}
